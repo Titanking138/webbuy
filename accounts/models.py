500a9daf7a9465f7d8291352ba6e218a967999d3
Titanking138 <arjunamaniya@gmail.com>
2022-01-19 23:52:50 +0530

    user model

--- accounts/models.py
+++ accounts/models.py
@@ -56,6 +56,8 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD  = 'email'
     REQUIRED_FIELDS = ['username','first_name','last_name']
 
+    # readonly_fields = ('date_joined')
+
     objects = MyAccountManager()
 
     def __str__(self):
