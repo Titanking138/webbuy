c7f1425996cc361149079a5dae59eff5fb46fda9
kevla-4121 <70936620+kevla-4121@users.noreply.github.com>
2022-01-27 09:31:50 +0530

    asd

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from itertools import product
-from tkinter import N
+from logging import exception
+from tkinter import E, N
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 from category.models import Category
@@ -22,4 +23,17 @@ def store(request, category_slug=None):
         'product_count': product_count,
     }
     return render(request , 'store/store.html' , context)
-    # return render(request , 'store/store.html' )
+
+def product_detail(request , category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug= category_slug, slug= product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_product': single_product,
+    }
+    return render(request , 'store/product_detail.html' , context)
+
+   
+    
