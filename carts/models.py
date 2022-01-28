16cb5767639a6edb0bbf59c9438e20f4c4c3dd5e
Titanking138 <arjunamaniya@gmail.com>
2022-01-28 17:34:06 +0530

    final 7

--- carts/models.py
+++ carts/models.py
@@ -16,11 +16,11 @@ class Cart(models.Model):
 class CartItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     cart    = models.ForeignKey(Cart,on_delete=models.CASCADE)
-    quantity= models.IntegerField()
+    quantity  = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
     def sub_total(self):
-        return self.product.price * self.quantity
+        return self.product.price * self.quantity 
 
     def __str__(self):
         return self.product 
