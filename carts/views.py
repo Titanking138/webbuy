e5c83bc4114c33f9c3f0678721212ede0426fde4
kevla-4121 <70936620+kevla-4121@users.noreply.github.com>
2022-02-03 13:00:50 +0530

    Get instance part 2

--- carts/views.py
+++ carts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render , get_object_or_404
 from django.template import context
 from carts.models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product , Variation
 
 
 # Create your views here.
@@ -15,12 +15,20 @@ def _cart_id(request):
     return cart
 
 def add_cart(request,product_id):
-    if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
-
-    print(color , size)
     product = Product.objects.get(id=product_id) #get the product
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+                key = item
+                value = request.POST[key]
+
+                try:
+                    variation = Variation.objects.get(product = product, variation_category__iexact=key, variation_value__iexact=value)
+                    product_variation.append(variation)
+                except:
+                    pass
+
+   
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart id using httpsession method
     except Cart.DoesNotExist:
